0c3e1fe8986867894d7366cf6835fd4807176ff0
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2019-10-26 12:54:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,13 +70,13 @@ class Chase extends Component {
 }
 
 render((<Router>
-        <Switch>
+
     <Route path="/" component={App} />
     <Route path="/work-details-of-sai-charan-r-patlolla" component={Work} />
     <Route path="/contact-sai-charan-r-patlolla" component={Contact} />
     <Route path="/resume-of-sai-charan-r-patlolla" component={Resume} />
     <Route path="/search-technical-skills" component={Chase} />
-        </Switch>
+
 </Router>), document.getElementById('root'));
 
 
